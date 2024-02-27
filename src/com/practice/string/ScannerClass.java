47d8420b8b7948bb38ad7ea0ca94e5e90ffc9b11
Naveenchandardev <chandarnaveen88@gmail.com>
2024-02-27 22:29:41 +0530

    #scannerClass; nextLine() importance;

--- src/com/practice/string/ScannerClass.java
+++ src/com/practice/string/ScannerClass.java
@@ -5,20 +5,19 @@ import java.util.Scanner;
 public class ScannerClass {
     public static void main(String[] args) {
         System.out.println("Enter integer:");
-        Scanner obj = new Scanner(System.in);
-        int i = obj.nextInt();
-        System.out.println("you entered : "+i);
-        //obj.nextLine();
-
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
         System.out.println("Enter double:");
-        double doub = obj.nextDouble();
-        System.out.println("you entered : "+doub);
-        obj.nextLine(); // if you have next input is string; use nextline()
-        System.out.println("Enter String:");
-        String str = obj.nextLine();
-        System.out.println("you entered : "+str);
-        obj.close();
+        double d = scan.nextDouble();
+        System.out.println("Enter string:");
+        String  s = scan.nextLine();
+        scan.close();
 
 
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
