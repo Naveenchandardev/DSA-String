0d89a048f4c449f03a1704fa2bf61af36a78a8ef
Naveenchandardev <chandarnaveen88@gmail.com>
2024-03-02 21:19:54 +0530

    #unique charactors count

--- src/com/practice/string/SubString.java
+++ src/com/practice/string/SubString.java
@@ -1,23 +1,29 @@
 package com.practice.string;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 public class SubString {
 	public static void main(String[] args) {
-		String inputString ="chandde"; //count only unique characters
-		int start =0; int end =0; int max =0;
-		List<Character> result = new ArrayList<Character>();
-		while(end<inputString.length()) {
-			if(!result.contains(inputString.charAt(end))) {
-				result.add(inputString.charAt(end));
-				end++;
-				max = Math.max(max, result.size());
+		String input = "Hello World!";
+		Set<Character> uniqueChars = new HashSet<>();
+
+		// Iterate through each character in the input string
+		for (char x : input.toCharArray()) {
+			// Add the character to the set if it's not already present
+			if (!Character.isWhitespace(x)) {
+				uniqueChars.add(Character.toLowerCase(x));
 			}
-			else {
-				result.remove(Character.valueOf(inputString.charAt(start)));
-				start++;
- 			}
 		}
-		System.out.println(max);
+
+		// Print the unique characters
+		System.out.println("Unique Characters:");
+		for (char c : uniqueChars) {
+			System.out.print(c + " ");
+		}
+		System.out.println();
+		System.out.println("Count: " + uniqueChars.size());
 	}
 
 }
