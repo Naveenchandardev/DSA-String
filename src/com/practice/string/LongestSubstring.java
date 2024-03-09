a4c75d8d6a2eb9132d844ff96b990baeda9407c7
Naveenchandardev <chandarnaveen88@gmail.com>
2024-03-09 21:06:12 +0530

    #LONGESTsubstring;

---
+++ src/com/practice/string/LongestSubstring.java
@@ -0,0 +1,24 @@
+package com.practice.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String a ="ASDCSEFDHGYHJYT";String length="";
+        String output ="";
+        for(char c :a.toCharArray()){
+            if(!output.contains(String.valueOf(c))){
+                output=	output.concat(String.valueOf(c));
+            }
+            else{
+                int found = output.indexOf(c)+1;
+                output = output.substring(found,output.length());
+                output=	output.concat(String.valueOf(c));
+            }
+            length = length.length()>output.length()?length:output;
+            System.out.println(output);
+            System.out.println("longest substring :: "+length);
+        }
+    }
+}
