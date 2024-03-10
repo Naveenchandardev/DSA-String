831d8d2f9624eda8dae54d71224d6f29cf01a357
Naveenchandardev <chandarnaveen88@gmail.com>
2024-03-10 11:19:21 +0530

    #find date and day;

---
+++ src/com/practice/string/FindDay.java
@@ -0,0 +1,17 @@
+package com.practice.string;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class FindDay {
+    public static void main(String[] args) {
+        LocalDate givenDate = LocalDate.of(2024, 1, 1); // Assuming January 1, 2024 as the given date
+        DayOfWeek dayOfWeek = givenDate.getDayOfWeek();
+        System.out.println("Day of the week for the given date is: " + dayOfWeek.getDisplayName(TextStyle.FULL, Locale.ENGLISH));
+
+        LocalDate newDate = givenDate.plusDays(200);
+        System.out.println("200th day after the given date is: " + newDate);
+    }
+}
