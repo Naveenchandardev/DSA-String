c5202da02257d28ad678cfacdd9a9fe495feb5d7
Naveenchandardev <chandarnaveen88@gmail.com>
2024-03-15 15:21:52 +0530

    #pattern programs - up_down_triangle;

--- src/com/practice/pattern/Triangles.java
+++ src/com/practice/pattern/Triangles.java
@@ -2,15 +2,31 @@ package com.practice.pattern;
 
 public class Triangles {
     public static void main(String[] args) {
-        int a =5;
-        pring_no_of_rows_desc(a);
+        int a = 5;
+        //right_triangle(a);
+        up_down_triangle(a);
     }
 
-    private static void pring_no_of_rows_desc(int a) {
+    private static void up_down_triangle(int a) {
         System.out.println("print_no_of_rows_asc");
-        for(int i=1;i<=2*a;i++){
-            int check = i>a? 2*a -i:i;
-            for(int j=1;j<=check;j++){
+        for (int i = 1; i <= 2 * a; i++) {
+            int check = i > a ? 2 * a - i : i;
+            int spaces = a - check;
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= check; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void right_triangle(int a) {
+        System.out.println("print_no_of_rows_asc");
+        for (int i = 1; i <= 2 * a; i++) {
+            int check = i > a ? 2 * a - i : i;
+            for (int j = 1; j <= check; j++) {
                 System.out.print("* ");
             }
             System.out.println();
