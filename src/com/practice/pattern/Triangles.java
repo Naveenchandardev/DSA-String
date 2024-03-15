588ea9eaeb112159f614298bba1fd383b93732c5
Naveenchandardev <chandarnaveen88@gmail.com>
2024-03-15 15:23:09 +0530

    #pattern programs - up_down_triangle-spelling change;

--- src/com/practice/pattern/Triangles.java
+++ src/com/practice/pattern/Triangles.java
@@ -3,12 +3,12 @@ package com.practice.pattern;
 public class Triangles {
     public static void main(String[] args) {
         int a = 5;
-        //right_triangle(a);
+        right_triangle(a);
         up_down_triangle(a);
     }
 
     private static void up_down_triangle(int a) {
-        System.out.println("print_no_of_rows_asc");
+        System.out.println("up_down_triangle");
         for (int i = 1; i <= 2 * a; i++) {
             int check = i > a ? 2 * a - i : i;
             int spaces = a - check;
@@ -23,7 +23,7 @@ public class Triangles {
     }
 
     private static void right_triangle(int a) {
-        System.out.println("print_no_of_rows_asc");
+        System.out.println("right_triangle");
         for (int i = 1; i <= 2 * a; i++) {
             int check = i > a ? 2 * a - i : i;
             for (int j = 1; j <= check; j++) {
