246b4e3772d49f51a68cbad7db8acd150d59dd38
Naveenchandardev <chandarnaveen88@gmail.com>
2024-04-06 16:23:25 +0530

    #swapping 2 integers

---
+++ src/com/practice/string/Swap_2_pointer.java
@@ -0,0 +1,11 @@
+package com.practice.string;
+
+public class Swap_2_pointer {
+    public static void main(String[] args) {
+        int a =5;int b=6;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a +" "+b);
+    }
+}
