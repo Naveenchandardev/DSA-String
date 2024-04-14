c83792dab8d20949f037a2fb84e8987ca20ff6cb
Naveenchandardev <chandarnaveen88@gmail.com>
2024-04-14 11:55:55 +0530

    #pattern; diamond patter;

--- src/com/practice/pattern/Diamond.java
+++ src/com/practice/pattern/Diamond.java
@@ -1,15 +1,18 @@
 package com.practice.pattern;
 
-public class Triangles {
+public class Diamond {
     public static void main(String[] args) {
-        int a = 5;
-        right_triangle(a);
+        int a = 10;
+        up_down_triangle(a);
     }
-
-    private static void right_triangle(int a) {
-        System.out.println("right_triangle");
+    private static void up_down_triangle(int a) {
+        System.out.println("up_down_triangle");
         for (int i = 1; i <= 2 * a; i++) {
             int check = i > a ? 2 * a - i : i;
+            int spaces = a - check;
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= check; j++) {
                 System.out.print("* ");
             }
--- src/com/practice/string/FirstNonRepeatingChar.java
+++ src/com/practice/string/FirstNonRepeatingChar.java
@@ -4,10 +4,10 @@ public class FirstNonRepeatingChar {
     public static void main(String[] args) {
         //FIND first non repeating char;
         String a="zsdczxs";
-        System.out.println(findss(a));
+        System.out.println(solution(a));
     }
 
-    private static Character findss(String a) {
+    private static Character solution(String a) {
         for(char c: a.toCharArray()){
             if (a.indexOf(c) == a.lastIndexOf(c))
                 return c;
