081d8f4261a07d5bf440b6c58fa1a58fc906d184
Naveenchandardev <chandarnaveen88@gmail.com>
2024-04-19 10:25:18 +0530

    #geeksforGeeks; find longest substring;

---
+++ src/com/practice/string/LongestSubstring_2.java
@@ -0,0 +1,28 @@
+package com.practice.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstring_2 {
+    public static void main(String[] args) {
+        String input = "chandar";
+        int result = lengthOfLongestSubstring(input);
+        System.out.println("Length of the longest substring without repeating characters: " + result);
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty())
+            return 0;
+        int maxLength = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
+            }
+            map.put(c, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
