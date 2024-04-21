1296b82c0064e269f333067523a4b3b6dccb043b
Naveenchandardev <chandarnaveen88@gmail.com>
2024-04-21 16:15:58 +0530

    #geeksforgeeks; insert character at particular position

---
+++ src/com/practice/string/InsertCharacter.java
@@ -0,0 +1,25 @@
+package com.practice.string;
+
+import java.util.Arrays;
+
+public class InsertCharacter {
+    public static void main(String[] args) {
+        String str="geeksforgeek";
+        int []a={1,5,7};
+        String res = solution(str,a);
+        System.out.println(res);
+    }
+
+    private static String solution(String str, int[] a) {
+        StringBuilder res = new StringBuilder();
+        String arraystring = Arrays.toString(a);
+        for( int i=0; i<str.length();i++){
+            if(arraystring.contains(String.valueOf(i))){
+                res.append("*");
+            }
+            else
+                res.append(str.charAt(i));
+        }
+        return res.toString();
+    }
+}
